2715193051e0fa518ac6422a9abf35c96cb8830b
Witold <witekrychlik@o2.pl>
2023-06-01 22:34:28 +0200

    html parser added

---
+++ src/app/blog/[slug]/components/PostGallery.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import styles from '../Post.module.scss';
+
+type Image = {
+  src: string,
+  alt: string
+};
+
+export default function ImageGallery({ images }: { images: Image[] }) {
+  if (!images) {
+    return <div>No images yet.</div>;
+  }
+  return (
+    <div className={styles.post__gallery}>
+      {images.map((image) => (
+        <img key={image.src} className={styles['post__gallery-image']} src={image.src} alt={image.alt} />
+      ))}
+    </div>
+  );
+}
