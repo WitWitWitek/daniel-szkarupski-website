c39efee82bc4a0d195b293ed3180e7915aeabb77
Witold <witekrychlik@o2.pl>
2023-06-06 20:43:09 +0200

    fetching data of aboutme page moved to page tsx

--- src/app/components/AboutMe/AboutMe.tsx
+++ src/app/components/AboutMe/AboutMe.tsx
@@ -1,15 +1,12 @@
 /* eslint-disable react/no-danger */
-import getAboutMePage from '@/app/lib/aboutMePage';
 import styles from './AboutMe.module.scss';
 
-export default async function AboutMe() {
-  const aboutMe = getAboutMePage();
-  const aboutMeData = await aboutMe;
+export default function AboutMe({ content }: { content: string }) {
   return (
     <section className={styles['about-me']}>
       <h2 id="o-mnie">O mnie</h2>
       <div className={styles['about-me__text']}>
-        <article dangerouslySetInnerHTML={{ __html: aboutMeData.html }} />
+        <article dangerouslySetInnerHTML={{ __html: content }} />
       </div>
     </section>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import ContactForm from './components/ContactForm/ContactForm';
 import HomeBanner from './components/HomeBanner/HomeBanner';
 import JoinUs from './components/JoinUs/JoinUs';
 import SocialMedia from './components/SocialMedia/SocialMedia';
+import getAboutMePage from './lib/aboutMePage';
 
-export default function Home() {
+export default async function Home() {
+  const aboutMe = getAboutMePage();
+  const { html } = await aboutMe;
   return (
     <>
       <HomeBanner />
-      <AboutMe />
+      <AboutMe content={html} />
       <JoinUs />
       <ContactForm />
       <SocialMedia />
