cb923119d8fbd6295a8d3c155ef2925cf19a6886
Witold <witekrychlik@o2.pl>
2023-06-06 20:36:58 +0200

    dynamic content of about me section added

--- src/app/components/AboutMe/AboutMe.tsx
+++ src/app/components/AboutMe/AboutMe.tsx
@@ -1,50 +1,15 @@
+/* eslint-disable react/no-danger */
+import getAboutMePage from '@/app/lib/aboutMePage';
 import styles from './AboutMe.module.scss';
 
-export default function AboutMe() {
+export default async function AboutMe() {
+  const aboutMe = getAboutMePage();
+  const aboutMeData = await aboutMe;
   return (
     <section className={styles['about-me']}>
       <h2 id="o-mnie">O mnie</h2>
       <div className={styles['about-me__text']}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur
-          maxime at, accusantium omnis architecto vero tenetur, cum perferendis,
-          {' '}
-          <span> assumenda ipsam fugit velit magni error provident quidem</span>
-          . Blanditiis, molestias? Ut eligendi aliquam cumque doloremque ratione sit.
-          Officia commodi enim deleniti sunt accusamus culpa exercitationem minima, amet
-          quia ratione magnam perferendis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          {' '}
-          <span>Sequi</span>
-          {' '}
-          aspernatur maxime at, accusantium omnis architecto vero tenetur,
-          cum perferendis, assumenda ipsam fugit velit magni error provident quidem.
-          Blanditiis, molestias? Ut eligendi aliquam cumque doloremque ratione sit. Officia
-          commodi enim deleniti sunt accusamus culpa exercitationem minima, amet quia ratione
-          magnam perferendis?
-        </p>
-        <p>
-          <span>Lorem ipsum dolor</span>
-          {' '}
-          sit amet consectetur adipisicing elit. Sequi aspernatur maxime at, accusantium
-          omnis architecto vero tenetur, cum perferendis, assumenda ipsam fugit velit
-          magni error provident quidem. Blanditiis, molestias? Ut eligendi aliquam cumque
-          doloremque ratione sit. Officia commodi enim deleniti sunt accusamus culpa
-          exercitationem minima, amet quia ratione magnam perferendis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur maxime
-          at, accusantium omnis architecto vero tenetur, cum perferendis, assumenda ipsam
-          fugit velit magni error provident quidem. Blanditiis, molestias? Ut eligendi
-          aliquam cumque doloremque ratione sit.
-          {' '}
-          <span>
-            Officia commodi enim deleniti sunt accusamus culpa exercitationem minima,
-            amet quia ratione magnam perferendis?
-          </span>
-        </p>
+        <article dangerouslySetInnerHTML={{ __html: aboutMeData.html }} />
       </div>
     </section>
   );
