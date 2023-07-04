bd8c0b868bbb4052e5c7a4de47915318cce0a13c
Witold <witekrychlik@o2.pl>
2023-07-04 20:46:22 +0200

    framer motion added

--- src/app/components/HomeBanner/HomeBanner.tsx
+++ src/app/components/HomeBanner/HomeBanner.tsx
@@ -1,25 +1,42 @@
+'use client';
+
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 import styles from './HomeBanner.module.scss';
+
+const container = {
+  whileInView: (i = 1) => ({
+    opacity: [0, 1],
+    transition: { staggerChildren: 0.2, delayChildren: 0.2 * i },
+  }),
+};
+
+const child = {
+  whileInView: {
+    opacity: [0, 1],
+    y: [20, 0],
+    transition: { duration: 0.8 },
+  },
+};
 
 export default function HomeBanner() {
   return (
     <section className={styles.banner}>
-      <div className={styles.banner__header}>
-        <h1>
+      <motion.div variants={container} animate="whileInView" className={styles.banner__header}>
+        <motion.h1 variants={child}>
           Daniel
-          <br />
-          {' '}
-          Szkarupski
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Maiores optio eius aliquid temporibus.
-        </p>
-        <div className={styles['banner__btn-container']}>
-          <a href="https://wesprzyjrn.pl/" className={styles['banner__support-btn']}>Wesprzyj</a>
-          <a href="/#dolacz-do-nas" className={styles['banner__join-btn']}>Dołącz do nas</a>
-        </div>
-      </div>
+          <br /> Szkarupski
+        </motion.h1>
+        <motion.p variants={child}>Bezpieczeństwo, Skuteczność, Tożsamość.</motion.p>
+        <motion.div variants={child} className={styles['banner__btn-container']}>
+          <a href="https://wesprzyjrn.pl/" className={styles['banner__support-btn']}>
+            Wesprzyj
+          </a>
+          <a href="/#dolacz-do-nas" className={styles['banner__join-btn']}>
+            Dołącz do nas
+          </a>
+        </motion.div>
+      </motion.div>
       <div className={styles.banner__image}>
         <Image src="/daniel-cover.png" alt="Daniel Szkarupski zdjęcie promocyjne" fill />
       </div>
--- src/app/components/JoinUs/JoinUs.tsx
+++ src/app/components/JoinUs/JoinUs.tsx
@@ -1,59 +1,59 @@
 import styles from './JoinUs.module.scss';
 import Person from './Person/Person';
 
-const teamArray: Person[] = [{
-  name: 'Bartłomiej Czuchnowski',
-  position: 'Rzecznik prasowy komitetu, koordynator rady programowej',
-  thumbnail: '/team/bartlomiej-czuchnowski.jpeg',
-},
-{
-  name: 'Dawid Gaczyński',
-  position: 'Szef sztabu wyborczego',
-  thumbnail: '/team/dawid-gaczynski.jpg',
-},
-{
-  name: 'Bartłomiej Lach',
-  position: 'Zastępca szefa sztabu wyborczego, pełnomocnik ds. finansowych',
-  thumbnail: '/team/bartlomiej-lach.jpg',
-},
-{
-  name: 'Karsten Glensk',
-  position: 'Specialista ds. marketingu',
-  thumbnail: '/team/karsten-glensk.jpg',
-},
-{
-  name: 'Kacper Lachowicz',
-  position: 'Koordynator działu logistycznego',
-  thumbnail: '/team/kacper-lachowicz.jpeg',
-},
-{
-  name: 'Łukasz Hałaszkiewicz',
-  position: 'Koordynator ds. wsparcia sympatyków',
-  thumbnail: '/team/lukasz-halaszkiewicz.jpg',
-},
-{
-  name: 'Romuald Kałwa',
-  position: 'Specialista ds. kontaktu z mediami',
-  thumbnail: '/team/romuald-kalwa.jpg',
-},
-{
-  name: 'Łukasz Pukas',
-  position: 'Koordynator zespołu graficznego',
-  thumbnail: '/team/lukasz-pukas.jpg',
-}];
+const teamArray: Person[] = [
+  {
+    name: 'Bartłomiej Czuchnowski',
+    position: 'Rzecznik prasowy komitetu, koordynator rady programowej',
+    thumbnail: '/team/bartlomiej-czuchnowski.jpeg',
+  },
+  {
+    name: 'Dawid Gaczyński',
+    position: 'Szef sztabu wyborczego',
+    thumbnail: '/team/dawid-gaczynski.jpg',
+  },
+  {
+    name: 'Bartłomiej Lach',
+    position: 'Zastępca szefa sztabu wyborczego, pełnomocnik ds. finansowych',
+    thumbnail: '/team/bartlomiej-lach.jpg',
+  },
+  {
+    name: 'Karsten Glensk',
+    position: 'Specialista ds. marketingu',
+    thumbnail: '/team/karsten-glensk.jpg',
+  },
+  {
+    name: 'Kacper Lachowicz',
+    position: 'Koordynator działu logistycznego',
+    thumbnail: '/team/kacper-lachowicz.jpeg',
+  },
+  {
+    name: 'Łukasz Hałaszkiewicz',
+    position: 'Koordynator ds. wsparcia sympatyków',
+    thumbnail: '/team/lukasz-halaszkiewicz.jpg',
+  },
+  {
+    name: 'Romuald Kałwa',
+    position: 'Specialista ds. kontaktu z mediami',
+    thumbnail: '/team/romuald-kalwa.jpg',
+  },
+  {
+    name: 'Łukasz Pukas',
+    position: 'Koordynator zespołu graficznego',
+    thumbnail: '/team/lukasz-pukas.jpg',
+  },
+];
 
 export default function JoinUs() {
   return (
     <section className={styles['join-us']}>
       <h2 id="dolacz-do-nas">
         Dołącz
-        <br />
-        {' '}
-        do nas!
+        <br /> do nas!
       </h2>
       <div className={styles['join-us__wrapper']}>
         {teamArray.map((person) => (
-          <Person key={person.name} teamMember={person} />
+          <Person key={person.name + Math.random()} teamMember={person} />
         ))}
       </div>
     </section>
