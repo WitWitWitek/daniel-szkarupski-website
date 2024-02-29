c34a0facecb2b43b8cf7a31f120ce42a51c73799
Witold <witekrychlik@o2.pl>
2024-02-29 21:26:21 +0100

    feat: sanity enviroment established

---
+++ src/app/sanity/schemaTypes/posts.ts
@@ -0,0 +1,48 @@
+import {defineField, Rule} from 'sanity'
+
+export default {
+  name: 'posts',
+  type: 'document',
+  title: 'Artykuły',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      title: 'Tytuł',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      title: 'Link do artykułu',
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      title: 'Data publikacji',
+      type: 'date',
+      name: 'releaseDate',
+    }),
+    defineField({
+      name: 'coverImage',
+      type: 'image',
+      title: 'Cover Image',
+    }),
+    defineField({
+      name: 'description',
+      type: 'text',
+      title: 'Opis',
+      validation: (Rule) => Rule.min(1).max(200),
+    }),
+    defineField({
+      name: 'content',
+      type: 'array',
+      title: 'Treść',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    }),
+  ],
+}
