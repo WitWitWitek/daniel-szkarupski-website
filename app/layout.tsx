20ef36c0e7ec43728e9cbba130745ee5a8831b90
Witold <witekrychlik@o2.pl>
2024-08-03 12:05:24 +0200

    feat: google-site-verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,33 @@
-import type { Metadata } from 'next';
-import Script from 'next/script';
-import Navbar from './components/Navbar/Navbar';
-import './globals.scss';
-import Footer from './components/Footer/Footer';
+import type { Metadata } from "next";
+import Script from "next/script";
+import Navbar from "./components/Navbar/Navbar";
+import "./globals.scss";
+import Footer from "./components/Footer/Footer";
 
 export const metadata: Metadata = {
-  title: 'Daniel Szkarupski - Ruch Narodowy & Konfederacja',
+  title: "Daniel Szkarupski - Ruch Narodowy & Konfederacja",
   description:
-    'Kandydat nr 2 Konfederacji do Sejmu RP w woj. opolskim. Konfederacja to jedyny antysystemowy wybór spoza układu okrągłego stołu (PiS, PO, SLD, PSL).',
+    "Kandydat nr 2 Konfederacji do Sejmu RP w woj. opolskim. Konfederacja to jedyny antysystemowy wybór spoza układu okrągłego stołu (PiS, PO, SLD, PSL).",
   keywords:
-    'polityka, samorząd, sejmik, patriotyzm, Ruch Narodowy, Konfederacja, patriotyzm, województwo opolskie, wybory, Opole, Nysa, Brzeg, Opolszczyzna',
+    "polityka, samorząd, sejmik, patriotyzm, Ruch Narodowy, Konfederacja, patriotyzm, województwo opolskie, wybory, Opole, Nysa, Brzeg, Opolszczyzna",
   openGraph: {
-    title: 'Daniel Szkarupski - Ruch Narodowy & Konfederacja',
+    title: "Daniel Szkarupski - Ruch Narodowy & Konfederacja",
     description:
-      'Kandydat nr 2 Konfederacji do Sejmu RP w woj. opolskim. Konfederacja to jedyny antysystemowy wybór spoza układu okrągłego stołu (PiS, PO, SLD, PSL).',
+      "Kandydat nr 2 Konfederacji do Sejmu RP w woj. opolskim. Konfederacja to jedyny antysystemowy wybór spoza układu okrągłego stołu (PiS, PO, SLD, PSL).",
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
+  },
+  verification: {
+    google: "KfG_6UiQAA_XZvxM_c2PGYluzLiGOmv9Re4rMR0Rals",
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
@@ -35,7 +42,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v16.0"
           nonce="lCqYkqwv"
         />
-        <Script async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />
+        <Script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charSet="utf-8"
+        />
       </body>
     </html>
   );
