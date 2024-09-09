dd6889acfea7eb416a76d59ce0b1170f368976be
Witold <witekrychlik@o2.pl>
2024-09-09 21:20:07 +0200

    fix: footer updated to avoid repetitions

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,32 +1,37 @@
-import Link from 'next/link';
-import styles from './Footer.module.scss';
-import SocialMediaTiles from '../SocialMedia/SocialMediaTiles/SocialMediaTiles';
+import Link from "next/link";
+import styles from "./Footer.module.scss";
+import SocialMediaTiles from "../SocialMedia/SocialMediaTiles/SocialMediaTiles";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__wrapper}>
-        <div className={styles['footer__user-container']}>
-          <h2 className={styles['footer__user-title']}>Daniel Szkarupski</h2>
-          <p className={styles['footer__user-description']}>
-            Zachęcam do śledzenia moich profili na mediach społecznościowych. Znajdziecie tam Państwo najświeższe
-            informacje dotyczące mojej działalności.
+        <div className={styles["footer__user-container"]}>
+          <h2 className={styles["footer__user-title"]}>daniel-szkarupski.pl</h2>
+          <p className={styles["footer__user-description"]}>
+            Zachęcam do śledzenia moich profili na mediach społecznościowych.
+            Znajdziecie tam Państwo najświeższe informacje dotyczące mojej
+            działalności.
           </p>
-          <div className={styles['footer__social-media']}>
+          <div className={styles["footer__social-media"]}>
             <SocialMediaTiles />
           </div>
         </div>
         <div className={styles.footer__menu}>
-          <div className={styles['footer__menu-container']}>
+          <div className={styles["footer__menu-container"]}>
             <h3>Ważne linki</h3>
             <a href="https://konfederacja.pl/" target="_blank" rel="noreferrer">
               Konfederacja
             </a>
-            <a href="https://ruch-narodowy.pl/" target="_blank" rel="noreferrer">
+            <a
+              href="https://ruch-narodowy.pl/"
+              target="_blank"
+              rel="noreferrer"
+            >
               Ruch Narodowy
             </a>
           </div>
-          <div className={styles['footer__menu-container']}>
+          <div className={styles["footer__menu-container"]}>
             <h3>Mapa Strony</h3>
             <a href="/#o-mnie">O mnie</a>
             <Link href="/blog">Blog</Link>
