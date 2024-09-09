deee92eea7474f1a9c159b364c707219aa5a1065
Witold <witekrychlik@o2.pl>
2024-09-09 20:22:31 +0200

    style: font size of submit button adjusted

--- app/components/Contact/ContactForm/ContactForm.tsx
+++ app/components/Contact/ContactForm/ContactForm.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import useContactForm from '../../../hooks/useContactForm';
-import styles from '../Contact.module.scss';
+import React, { useEffect, useState } from "react";
+import useContactForm from "../../../hooks/useContactForm";
+import styles from "../Contact.module.scss";
 
 export interface FormContent {
   name: string;
@@ -11,17 +11,19 @@ export interface FormContent {
   text: string;
 }
 
-type InputName = 'name' | 'email' | 'subject' | 'text';
+type InputName = "name" | "email" | "subject" | "text";
 
 export default function ContactForm() {
   const [formContent, setFormContent] = useState<FormContent>({
-    name: '',
-    email: '',
-    subject: '',
-    text: '',
+    name: "",
+    email: "",
+    subject: "",
+    text: "",
   });
   const { sendContactForm, formState } = useContactForm();
-  const onFormChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const onFormChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const name = e.target.name as InputName;
     setFormContent((prev) => ({
       ...prev,
@@ -37,17 +39,17 @@ export default function ContactForm() {
   useEffect(() => {
     if (formState.success) {
       setFormContent({
-        name: '',
-        email: '',
-        subject: '',
-        text: '',
+        name: "",
+        email: "",
+        subject: "",
+        text: "",
       });
     }
   }, [formState.success]);
 
   return (
-    <form onSubmit={onFormSubmission} className={styles['contact-form__form']}>
-      <label htmlFor="name" className={styles['contact-form__label']}>
+    <form onSubmit={onFormSubmission} className={styles["contact-form__form"]}>
+      <label htmlFor="name" className={styles["contact-form__label"]}>
         Imię i nazwisko:
         <input
           id="name"
@@ -56,11 +58,11 @@ export default function ContactForm() {
           maxLength={50}
           onChange={onFormChange}
           value={formContent.name}
-          className={styles['contact-form__input']}
+          className={styles["contact-form__input"]}
           required
         />
       </label>
-      <label htmlFor="email" className={styles['contact-form__label']}>
+      <label htmlFor="email" className={styles["contact-form__label"]}>
         E-mail:
         <input
           id="email"
@@ -69,12 +71,12 @@ export default function ContactForm() {
           maxLength={50}
           onChange={onFormChange}
           value={formContent.email}
-          className={styles['contact-form__input']}
+          className={styles["contact-form__input"]}
           required
         />
       </label>
 
-      <label htmlFor="subject" className={styles['contact-form__label']}>
+      <label htmlFor="subject" className={styles["contact-form__label"]}>
         Temat:
         <input
           id="subject"
@@ -83,12 +85,12 @@ export default function ContactForm() {
           maxLength={50}
           onChange={onFormChange}
           value={formContent.subject}
-          className={styles['contact-form__input']}
+          className={styles["contact-form__input"]}
           required
         />
       </label>
 
-      <label htmlFor="text" className={styles['contact-form__label']}>
+      <label htmlFor="text" className={styles["contact-form__label"]}>
         Opis:
         <textarea
           id="text"
@@ -96,22 +98,26 @@ export default function ContactForm() {
           maxLength={1000}
           onChange={onFormChange}
           value={formContent.text}
-          className={styles['contact-form__textarea']}
+          className={styles["contact-form__textarea"]}
           required
         />
       </label>
 
-      <button type="submit" disabled={formState.loading} className={styles['contact-form__button']}>
-        {!formState.loading ? 'Wyślij' : 'Wysyłam...'}
+      <button
+        type="submit"
+        disabled={formState.loading}
+        className={styles["contact-form__button"]}
+      >
+        {!formState.loading ? "Wyślij" : "Wysyłam..."}
       </button>
       {formState.success && !formState.error && (
-        <div className={styles['contact-form__success']}>
+        <div className={styles["contact-form__success"]}>
           <p>✓</p>
           <div>Wysłano poprawnie</div>
         </div>
       )}
       {!formState.success && formState.error && (
-        <div className={styles['contact-form__error']}>
+        <div className={styles["contact-form__error"]}>
           <p>X</p>
           <div>Błąd wysyłania forumlarza</div>
         </div>
